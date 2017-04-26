da271d65dbea5b8ba4cce9902def60779e9b03d8
Jessica <yanzeliu.france@gmail.com>
2017-04-25 18:32:14 -0700

    user log in

--- TummyOnTrack/TummyOnTrack/LoginViewController.swift
+++ TummyOnTrack/TummyOnTrack/LoginViewController.swift
@@ -7,8 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var emailLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,27 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func loginUserClicked(_ sender: Any) {
+        guard let email = emailLabel.text, let password = passwordLabel.text else {
+            print("Please enter valid login information")
+            return
+        }
+        loginUserWith(email: email, password: password)
+        
+    }
 
+    func loginUserWith(email: String, password: String) {
+        FIRAuth.auth()?.signIn(withEmail: email, password: password, completion: { (user: FIRUser?, error) in
+            if error != nil {
+                print("Login user error: \(error)")
+            }
+            
+            let mainStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let homeVC = mainStoryboard.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
+            self.present(homeVC, animated: true, completion: nil)
+        })
+    }
 
 }
