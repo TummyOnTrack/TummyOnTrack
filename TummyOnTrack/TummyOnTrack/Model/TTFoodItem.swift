99b12353903cfcfb0369378bfd814531d8cd9e77
Pooja Chowdhary <poojachow@PoojaBenake.local>
2017-04-28 08:55:20 -0700

    Updating FoodItem

--- TummyOnTrack/TummyOnTrack/Model/TTFoodItem.swift
+++ TummyOnTrack/TummyOnTrack/Model/TTFoodItem.swift
@@ -6,4 +6,27 @@
 //  Copyright © 2017 Gauri Tikekar. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class TTFoodItem {
+    var name: String?
+    var points: Int?
+    var tags: [String]?
+    var images: [URL]?
+    
+    init(dictionary: NSDictionary) {
+        name = dictionary["name"] as? String
+        points = dictionary["points"] as? Int
+        tags = dictionary["tags"] as? [String]
+        var imageurls = [URL]()
+        if let imagesdict = dictionary["images"] as? NSArray {
+            for imageurlstring in imagesdict {
+                if let imageurl = URL(string: imageurlstring as! String) {
+                    imageurls.append(imageurl)
+                }
+                
+            }
+            images = imageurls
+        }
+    }
+}
