7ab2c27d03d102a659d9a5c79602686493e74027
Gauri Tikekar <gauritikekar@gauris-mbp.lan>
2017-04-30 22:37:52 -0700

    Basic food game animation

--- TummyOnTrack/TummyOnTrack/Common/ViewControllers/TTGameViewController.swift
+++ TummyOnTrack/TummyOnTrack/Common/ViewControllers/TTGameViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class TTGameViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    var selectedFoodItems = [TTFoodItem]()
+    var selectedFoodString: String!
+    var defaultFoodNames = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        selectedFoodString = "apple banana asparagus aubergine"
+        checkForFoodItemsPresent()
+        collectionView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func checkForFoodItemsPresent() {
+        for food in TTFoodItem.defaultFoodList {
+            if let name = food.name?.lowercased() {
+                defaultFoodNames.append(name)
+            }
+        }
+        
+        if selectedFoodString != nil {
+            let selectedFoodArray = selectedFoodString.components(separatedBy: " ")
+            for selectedFood in selectedFoodArray {
+                _ = TTFoodItem.defaultFoodList.contains(where: { (food: TTFoodItem) -> Bool in
+                    if selectedFood == food.name?.lowercased() {
+                        selectedFoodItems.append(food)
+                        return true
+                    }
+                    else {
+                        return false
+                    }
+                })
+            }
+        }
     }
     
 
@@ -33,3 +61,46 @@ class TTGameViewController: UIViewController {
     */
 
 }
+
+// MARK: - UICollectionViewDataSource protocol
+extension TTGameViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return selectedFoodItems.count
+    }
+    
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GameCell", for: indexPath as IndexPath) as! TTGameCollectionViewCell
+        
+       cell.foodItem = selectedFoodItems[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let foodItem_ = selectedFoodItems[indexPath.row]
+        let cell =  collectionView.cellForItem(at: indexPath) as! TTGameCollectionViewCell
+        
+        if let imageurlstring = foodItem_.images?[0] {
+            if let data = try? Data(contentsOf: imageurlstring) {
+                let image_ = UIImage(data: data)
+                let imageView_ = UIImageView(image: image_)
+                imageView_.frame = CGRect(x: cell.frame.origin.x, y: cell.frame.origin.y, width: cell.foodImageView.frame.width, height: cell.foodImageView.frame.height)
+                view.addSubview(imageView_)
+                UIView.animate(withDuration: 0.7, animations: {
+                    imageView_.frame = CGRect(x: self.view.frame.width/2, y: self.view.frame.height - imageView_.frame.height - 70, width: imageView_.frame.width, height: imageView_.frame.height)
+                }, completion: { (Bool) in
+                    
+                })
+                
+            }
+        }
+    }
+    
+    
+}
+
