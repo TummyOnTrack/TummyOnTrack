5f90b2428eda1b5c779cea194fdcb310244788d4
Davis Wamola <dawa009@yahoo.com>
2017-05-04 11:41:54 -0700

    Complete Rewards flow

--- TummyOnTrack/TummyOnTrack/Rewards/TTDecorateRoomViewController.swift
+++ TummyOnTrack/TummyOnTrack/Rewards/TTDecorateRoomViewController.swift
@@ -20,11 +20,16 @@ class TTDecorateRoomViewController: UIViewController {
     fileprivate var trayOriginalCenter: CGPoint!
     fileprivate var myRewards: [TTReward]?
 
+    fileprivate var originalRewardCenter: CGPoint!
+    fileprivate var newlyCreatedReward: UIImageView!
+    fileprivate var newlyCreatedRewardCenter: CGPoint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        trayOriginalCenter = trayView.center
         trayDownOffset = 120
-        trayUp = trayView.center
+        trayUp = trayOriginalCenter
         trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y - trayDownOffset)
 
         myRewards = TTProfile.currentProfile?.rewards
@@ -39,11 +44,14 @@ class TTDecorateRoomViewController: UIViewController {
 
             for myReward in myRewards {
                 let frame = CGRect(x: x, y: y, width: 50, height: 50)
-                let imageView = UIImageView(frame: frame)
-                imageView.setImageWith(myReward.thumbnailURL!)
-                imageView.isUserInteractionEnabled = true
+                let myRewardImageView = UIImageView(frame: frame)
+                myRewardImageView.setImageWith(myReward.thumbnailURL!)
+                myRewardImageView.isUserInteractionEnabled = true
 
-                trayView.addSubview(imageView)
+                let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPanReward(sender:)))
+                myRewardImageView.addGestureRecognizer(panGestureRecognizer)
+
+                trayView.addSubview(myRewardImageView)
                 x += xOffset
 
                 // If the next image's right edge will be past the frame
@@ -62,7 +70,7 @@ class TTDecorateRoomViewController: UIViewController {
     
     @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
         if sender.state == .began {
-            trayOriginalCenter = trayView.center
+
         } else if sender.state == .changed {
 
         } else if sender.state == .ended {
@@ -81,5 +89,77 @@ class TTDecorateRoomViewController: UIViewController {
                 }, completion: nil)
             }
         }
+    }
+}
+
+extension TTDecorateRoomViewController: UIGestureRecognizerDelegate {
+
+    func didPanReward(sender: UIPanGestureRecognizer) {
+        let originalRewardImageView = sender.view as! UIImageView
+        let point = sender.location(in: view)
+        var imageViewToPan = originalRewardImageView
+        if sender.state == .began {
+            originalRewardCenter = originalRewardImageView.center
+
+            if point.y > self.trayView.frame.origin.y {
+                // Pan gesture on reward in the tray, create a copy to move
+                newlyCreatedReward = UIImageView(image: originalRewardImageView.image)
+                let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPanReward(sender:)))
+                let pinchGestureRecognizer = UIPinchGestureRecognizer(target: self, action: #selector(didPinchReward(sender:)))
+
+                // Attach it to the reward's image view and enable user interaction
+                newlyCreatedReward.isUserInteractionEnabled = true
+                newlyCreatedReward.addGestureRecognizer(panGestureRecognizer)
+                newlyCreatedReward.addGestureRecognizer(pinchGestureRecognizer)
+
+                pinchGestureRecognizer.delegate = self;
+
+                imageViewToPan = newlyCreatedReward
+            } else {
+                newlyCreatedReward = nil
+            }
+        } else if sender.state == .changed {
+            let translation = sender.translation(in: view)
+            UIView.animate(withDuration: 0.3, animations: {
+                imageViewToPan.center = CGPoint(x: self.originalRewardCenter.x + translation.x, y: self.originalRewardCenter.y + translation.y)
+            })
+        } else if sender.state == .ended {
+            if point.y > self.trayView.frame.origin.y {
+                // Pan gesture ended inside the tray, return the image view to it's original location
+                UIView.animate(withDuration: 0.7) {
+                    originalRewardImageView.center = self.originalRewardCenter
+                    if self.newlyCreatedReward != nil {
+                        // Remove the newly created view
+                        self.newlyCreatedReward.removeFromSuperview()
+                    }
+                }
+            } else if newlyCreatedReward != nil {
+                // Pan gesture ended outside the tray, add the newly created view to the superview
+                newlyCreatedReward.center = originalRewardImageView.center
+
+                // Since the original reward is in the tray, but the new reward is in the
+                // main view, you have to offset the coordinates
+                newlyCreatedReward.center.y += trayView.frame.origin.y
+
+                UIView.animate(withDuration: 0.7) {
+                    originalRewardImageView.center = self.originalRewardCenter
+                    self.newlyCreatedReward.center = sender.location(in: self.view)
+                    self.view.addSubview(self.newlyCreatedReward)
+                }
+            }
+        }
+    }
+
+    func didPinchReward(sender: UIPinchGestureRecognizer) {
+        let point = sender.location(in: view)
+        print("Pinch Gesture began at: \(point)")
+        let scale = sender.scale
+        let rewardImageView = sender.view as! UIImageView
+        rewardImageView.transform = rewardImageView.transform.scaledBy(x: scale, y: scale)
+        sender.scale = 1
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 }
