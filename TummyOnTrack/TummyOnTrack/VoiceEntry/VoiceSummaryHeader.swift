8c0634d25825d0294f6b9f4f831d7e04fbfcd111
Pooja Chowdhary <poojachow@PoojaBenake.local>
2017-05-04 18:18:35 -0700

    Adding header to voice summary

---
+++ TummyOnTrack/TummyOnTrack/VoiceEntry/VoiceSummaryHeader.swift
@@ -0,0 +1,14 @@
+//
+//  VoiceSummaryHeader.swift
+//  TummyOnTrack
+//
+//  Created by Pooja Chowdhary on 5/4/17.
+//  Copyright © 2017 Gauri Tikekar. All rights reserved.
+//
+
+import UIKit
+
+class VoiceSummaryHeader: UICollectionReusableView {
+    @IBOutlet weak var totalPointsLabel: UILabel!
+        
+}
